423c5685d5ba539edd00413eab743c803137d825
zhou274 <274891948@qq.com>
2024-12-24 12:12:11 +0800

    1.2

--- Fall Guy/Assets/Scripts/GameWinScene.cs
+++ Fall Guy/Assets/Scripts/GameWinScene.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using GamePolygon;
+using StarkSDKSpace;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
 
 public class GameWinScene : MonoBehaviour
 {
 
     public GameObject[] Players;
     GameObject SelectedPlayer;
-    
-
+    private StarkAdManager starkAdManager;
+    public string clickid;
     public void Start()
     {
         SoundManager.Instance.StopMusic();
@@ -39,6 +42,86 @@ public class GameWinScene : MonoBehaviour
     }
     public void AddCoins()
     {
-        CoinManager.Instance.AddCoins(100);
+        ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
+                    
+                    Debug.Log("xxx");
+                    CoinManager.Instance.AddCoins(100);
+                    clickid = "";
+                    getClickid();
+                    apiSend("game_addiction", clickid);
+                    apiSend("lt_roi", clickid);
+
+
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+        //CoinManager.Instance.AddCoins(100);
+    }
+    public void getClickid()
+    {
+        var launchOpt = StarkSDK.API.GetLaunchOptionsSync();
+        if (launchOpt.Query != null)
+        {
+            foreach (KeyValuePair<string, string> kv in launchOpt.Query)
+                if (kv.Value != null)
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + kv.Value);
+                    if (kv.Key.ToString() == "clickid")
+                    {
+                        clickid = kv.Value.ToString();
+                    }
+                }
+                else
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + "null ");
+                }
+        }
+    }
+
+    public void apiSend(string eventname, string clickid)
+    {
+        TTRequest.InnerOptions options = new TTRequest.InnerOptions();
+        options.Header["content-type"] = "application/json";
+        options.Method = "POST";
+
+        JsonData data1 = new JsonData();
+
+        data1["event_type"] = eventname;
+        data1["context"] = new JsonData();
+        data1["context"]["ad"] = new JsonData();
+        data1["context"]["ad"]["callback"] = clickid;
+
+        Debug.Log("<-data1-> " + data1.ToJson());
+
+        options.Data = data1.ToJson();
+
+        TT.Request("https://analytics.oceanengine.com/api/v2/conversion", options,
+           response => { Debug.Log(response); },
+           response => { Debug.Log(response); });
+    }
+
+
+    /// <summary>
+    /// </summary>
+    /// <param name="adId"></param>
+    /// <param name="closeCallBack"></param>
+    /// <param name="errorCallBack"></param>
+    public void ShowVideoAd(string adId, System.Action<bool> closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            starkAdManager.ShowVideoAdWithId(adId, closeCallBack, errorCallBack);
+        }
     }
 }
--- Fall Guy/Assets/Scripts/MenuManager.cs
+++ Fall Guy/Assets/Scripts/MenuManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using GamePolygon;
+using StarkSDKSpace;
+using TTSDK.UNBridgeLib.LitJson;
+using TTSDK;
+
 public class MenuManager : MonoBehaviour {
 
 
 	public Text CurrentLevelText;
-   
+    private StarkAdManager starkAdManager;
 
     int CurrentLevel;
-	
 
-	// Use this for initialization
-	void Start () {
+    public string clickid;
+    // Use this for initialization
+    void Start () {
         SoundManager.Instance.StopMusic();
 
         SoundManager.Instance.PlayMusic(SoundManager.Instance.Menu);
@@ -32,7 +36,30 @@ public class MenuManager : MonoBehaviour {
 
     public void AddCoins()
     {
-        CoinManager.Instance.AddCoins(100);
+        ShowVideoAd("192if3b93qo6991ed0",
+            (bol) => {
+                if (bol)
+                {
+                    
+                    Debug.Log("观看完毕 获得奖励");
+                    CoinManager.Instance.AddCoins(100);
+                    clickid = "";
+                    getClickid();
+                    apiSend("game_addiction", clickid);
+                    apiSend("lt_roi", clickid);
+
+
+                }
+                else
+                {
+                    StarkSDKSpace.AndroidUIManager.ShowToast("观看完整视频才能获取奖励哦！");
+                }
+            },
+            (it, str) => {
+                Debug.LogError("Error->" + str);
+                //AndroidUIManager.ShowToast("广告加载异常，请重新看广告！");
+            });
+        //CoinManager.Instance.AddCoins(100);
     }
 
     public void Restart()
@@ -52,8 +79,56 @@ public class MenuManager : MonoBehaviour {
         SceneManager.LoadScene("Shop");
 
     }
+    public void getClickid()
+    {
+        var launchOpt = StarkSDK.API.GetLaunchOptionsSync();
+        if (launchOpt.Query != null)
+        {
+            foreach (KeyValuePair<string, string> kv in launchOpt.Query)
+                if (kv.Value != null)
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + kv.Value);
+                    if (kv.Key.ToString() == "clickid")
+                    {
+                        clickid = kv.Value.ToString();
+                    }
+                }
+                else
+                {
+                    Debug.Log(kv.Key + "<-参数-> " + "null ");
+                }
+        }
 
-   
+    }
+    public void apiSend(string eventname, string clickid)
+    {
+        TTRequest.InnerOptions options = new TTRequest.InnerOptions();
+        options.Header["content-type"] = "application/json";
+        options.Method = "POST";
+
+        JsonData data1 = new JsonData();
+
+        data1["event_type"] = eventname;
+        data1["context"] = new JsonData();
+        data1["context"]["ad"] = new JsonData();
+        data1["context"]["ad"]["callback"] = clickid;
+
+        Debug.Log("<-data1-> " + data1.ToJson());
+
+        options.Data = data1.ToJson();
+
+        TT.Request("https://analytics.oceanengine.com/api/v2/conversion", options,
+           response => { Debug.Log(response); },
+           response => { Debug.Log(response); });
+    }
+    public void ShowVideoAd(string adId, System.Action<bool> closeCallBack, System.Action<int, string> errorCallBack)
+    {
+        starkAdManager = StarkSDK.API.GetStarkAdManager();
+        if (starkAdManager != null)
+        {
+            starkAdManager.ShowVideoAdWithId(adId, closeCallBack, errorCallBack);
+        }
+    }
 
 
 
